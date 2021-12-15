7ad3fec36e8b880acce0e9ddc6628dbfeef565c4
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-12-15 15:48:01 +0700

    init

---
+++ src/handlers/hello.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Hello, world!"
+        })
+    }
+}
